5451679a70c0610f1f54a1a2995cad5516d6cb03
Robert Wójtowicz <rkwojtowicz@gmail.com>
2020-10-04 12:11:39 +0200

    1.3

--- import_mqtt.sh
+++ import_mqtt.sh
@@ -1,5 +1,5 @@
 # *********
-# Mi Body Composition Scale 2 Garmin Connect v1.2
+# Mi Body Composition Scale 2 Garmin Connect v1.3
 # *********
 
 # Data acquisition from MQTT broker
@@ -14,6 +14,7 @@ sed -i '1iWeight,Impedance,Units,User,Timestamp,Bat_in_V,Bat_in_%' $path/data/te
 sed -Ei '2,$ s/(([^,]*,){4})([^,]+)(.*)/echo \x27\1\x27$(date -d "\3" +%s)\x27\4\x27/e' $path/data/temp.log
 rename=`awk -F "\"*,\"*" 'END{print $5}' $path/data/temp.log`
 mv $path/data/temp.log $path/data/import_$rename.log
+
 if [ -f $path/data/export_$rename.log ] ; then
 	rm $path/data/import_$rename.log
 fi
@@ -22,10 +23,14 @@ fi
 python3 $path/export_garmin.py > $path/data/temp.log 2>&1
 
 # Logging and handling errors
+move=`awk -F "_" '/Processed file: import/{print $2;}' $path/data/temp.log`
+
 if grep -q 'Error' $path/data/temp.log ; then
 	echo 'Errors have been detected'
-elif [ -f $path/data/import_$rename.log ] ; then
-	mv $path/data/import_$rename.log $path/data/export_$rename.log
+elif grep -q 'panic' $path/data/temp.log ; then
+	echo 'Errors have been detected'
+elif [ -f $path/data/import_$move ] ; then
+	mv $path/data/import_$move $path/data/export_$move
 else
 	echo 'There is no file to export'
 fi
